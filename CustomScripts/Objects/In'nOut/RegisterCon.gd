7959d2f59745f97a78922befdbffeed6fd31ac5e
OctoHato <34582182+OctoHato@users.noreply.github.com>
2025-01-31 07:05:29 -0600

    Fryer and Item Checking Implemented

--- CustomScripts/Objects/In'nOut/RegisterCon.gd
+++ CustomScripts/Objects/In'nOut/RegisterCon.gd
@@ -3,16 +3,60 @@ var currentID
 var currentMark
 var currentNPC
 var SignalBusKOM
+var NpcInventory
 @export var NavNodeTarget : Node
 @export var SoundSource : AudioStreamPlayer
-@export var sound : AudioStream
+@export var SoundPositive : AudioStream
+@export var SoundNegative : AudioStream
 @export var PosRefrence : Node3D
+@export var FoodList : Dictionary
+@export var NeededAmounts : Array[int]
+var NeededTotal : int
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SignalBusKOM = get_tree().get_first_node_in_group("player").get_node("KOMSignalBus")
 	pass # Replace with function body.
 
 
+func NpcInvCheck():
+	var NeededTotal = 0
+	currentNPC = find_closest_or_furthest(PosRefrence,"PompNPC")
+	currentID = currentNPC.InstID
+	currentMark = get_tree().get_first_node_in_group("NavMark" + str(currentID))
+	print_rich("Register Current ID: [color=red]" + str(currentID) + "[/color]")
+	for i in get_all_children(get_tree().get_root()):
+		if i.is_in_group("PompNPC"):
+			if i.InstID == currentID:
+				for ii in i.get_children():
+					if ii.name == "InventoryGrid":
+						NpcInventory = ii
+	for i in FoodList.keys():
+		FoodList[i] = 0
+	
+	for i in NpcInventory.get_children():
+		var iterant = -1
+		if "prototype_id" in i:
+			for ii in FoodList.size():
+				iterant += 1
+				print("ItemID is: " + str(i.prototype_id) + " " + "FoodList Key is: " + str(FoodList.keys()[iterant]))
+				if i.prototype_id == FoodList.keys()[iterant]:
+					print("Match!")
+					FoodList[FoodList.keys()[iterant]] += 1
+					print(str(FoodList.values()))
+	var TotalItems = 0
+	for i in FoodList.size():
+		if FoodList.values()[i] >= NeededAmounts[i]:
+			TotalItems += 1
+		else:
+			print(str("not enough " + str(FoodList.values()[i])))
+	for i in NeededAmounts:
+		NeededTotal += NeededAmounts[i]
+		
+	print(str(TotalItems) + " " + str(NeededTotal))
+	if TotalItems >= NeededTotal:
+		return true
+	else:
+		return false
 
 func Task():
 	currentNPC = find_closest_or_furthest(PosRefrence,"PompNPC")
@@ -59,6 +103,11 @@ func get_all_children(in_node, array := []):
 
 
 func _on_pressed():
-	SoundSource.stream = sound
-	SoundSource.play()
-	Task()
+	
+	if NpcInvCheck() == true:
+		SoundSource.stream = SoundPositive
+		SoundSource.play()
+		Task()
+	else:
+		SoundSource.stream = SoundNegative
+		SoundSource.play()
